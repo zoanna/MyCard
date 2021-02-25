c5da04ca8003fcd4ccd67fd5b4b7bb27b224ac3b
Zoanna <ann.zozulya@gmail.com>
2021-02-25 19:44:14 +0200

    Initial Commit

---
+++ AnnaCard/AnnaCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnnaCardApp.swift
+//  AnnaCard
+//
+//  Created by Anna Zozulia on 25.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct AnnaCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
