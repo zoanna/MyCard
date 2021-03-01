8379231300a2bc9a3217cb6d99793fe56b56af33
Zoanna <ann.zozulya@gmail.com>
2021-03-01 17:25:29 +0200

    First commit

--- AnnaCard/ContentView.swift
+++ AnnaCard/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.61, green: 0.35, blue: 0.71, opacity: 1.00)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack {
+                Image("FullSizeRender")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 5))
+                
+                Text("Anna Zozulia")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .foregroundColor(.white)
+                    .bold()
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+380935070726", imageName: "phone.fill")
+                InfoView(text: "ann.zozulya@gmail.com", imageName: "envelope.fill")
+            }
+                
+        }
+        
     }
 }
 
@@ -19,3 +42,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
---
+++ AnnaCard/InfoView.swift
@@ -0,0 +1,33 @@
+//
+//  InfoView.swift
+//  AnnaCard
+//
+//  Created by Anna Zozulia on 25.02.2021.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 30)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(.all)
+    }
+}
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
